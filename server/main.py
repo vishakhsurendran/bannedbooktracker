44e767e97836156449bbac01d183e6236f5e8725
Coleton O'Donnell <contact@coleton.io>
2025-04-11 20:30:49 -0400

    Update main.py with CORSMiddleware, create axios config (from Rebecca)

--- server/main.py
+++ server/main.py
@@ -1,10 +1,20 @@
 import routing.books as books
-from database import Database
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.router.include_router(books.router, prefix="/books", tags=["Books"])
