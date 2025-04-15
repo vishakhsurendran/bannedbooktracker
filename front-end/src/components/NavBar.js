df21166405491b0a1f7cb490fd5173e1f21f0bd6
hwait123 <hwait12@ufl.edu>
2025-04-15 13:46:44 -0400

    Finished the nav bar and wrapped pages.

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,105 +1,75 @@
-/*
-import { Link } from 'react-router-dom';
 
-
-function NavBar() {
-  return (
-      <div>
-    <nav>
-      <ul className="navbar" style={{ listStyle: 'none', padding: 0, margin: 0 }}>
-        <li>
-            <img
-                src={book_icon}
-                alt="Banned Book Tracker Logo"
-                className="logo"/>
-        </li>
-        <li className="navbar-link">
-          <Link to="/" style={{ textDecoration: 'none' }}>HOME</Link>
-        </li>
-        <li className="navbar-link">
-          <Link to="/mybooks" style={{ textDecoration: 'none' }}>MY BOOKS</Link>
-        </li>
-        <li className="navbar-link">
-          <Link to="/browse" style={{ textDecoration: 'none' }}>BROWSE</Link>
-        </li>
-      </ul>
-    </nav>
-      <hr />
-          </div>
-  );
-}
-
-export default NavBar;*/
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom';
 import React from 'react';
 import "./NavBar.css";
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
+import ForgotPassword from "../pages/ForgotPassword";
+import SearchPage from "../pages/SearchPage";
+import BookDetails from "../pages/BookDetails";
+import Accounts from "../pages/Accounts";
+import Map from "../pages/Map";
+import LocationResult from "../pages/LocationResult";
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 import book_icon from "../pictures/book_icon.png";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import account_icon from "../pictures/account_icon.png";
+import message_icon from "../pictures/message_icon.png";
+import notif_icon from "../pictures/notif_icon.png";
 
 function NavBar() {
   return (
 
-    /*<Navbar expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img
-          src={book_icon}
-          alt="book icon"
-          className="logo"/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse >
-          <Nav >
-            <Nav.Link href="#home">HOME</Nav.Link>
-            <Nav.Link href="#link">MY BOOKS</Nav.Link>
-            <NavDropdown title="Dropdown" >
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>*/
-
+      <BrowserRouter>
+          <Container>
       <Navbar className="navbar">
-        <Navbar.Brand href="#home">
+        <Navbar.Brand href="/">
           <img
           src={book_icon}
           alt="book icon"
           className="logo"/>
         </Navbar.Brand>
-        <Nav.Link href="#home" className="navbar-link">HOME</Nav.Link>
-        <Nav.Link href="#mybooks" className="navbar-link">MY BOOKS</Nav.Link>
-        <NavDropdown title="BROWSE" className="navbar-link">
-              <NavDropdown.Item href="#action/3.1">GENRE 1</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                GENRE 2
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">GENRE 3</NavDropdown.Item>
-        </NavDropdown>
-          <Form inline>
-            <Form.Control
-              type="text"
+        <Link to="/" className="navbar-link">HOME</Link>
+        <Link to="/lists" className="navbar-link">LISTS</Link>
+          <Link to="/browse" className="navbar-link">BROWSE</Link>
+          <div className="spacer2"></div>
+          <input
+              type="search"
               placeholder="Search"
               className="navbar-search"
-            />
-      </Form>
+              style={{height: 30}}
+          />
+          <div className="spacer"></div>
+          <img
+                src={notif_icon}
+                alt="notification icon"
+                className="icon"/>
+          <img
+                src={message_icon}
+                alt="message icon"
+                className="icon"/>
+          <Link to="/accounts" style={{ alignSelf: "flex-end" }}>
+          <img
+                src={account_icon}
+                alt="account icon"
+                className="icon"/>
+          </Link>
       </Navbar>
-
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/reset" element={<ForgotPassword />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/book-details" element={<BookDetails />} />
+              <Route path="/accounts" element={<Accounts />} />
+              <Route path="/map" element={<Map />} />
+              <Route path="/location-result" element={<LocationResult />} />
+      </Routes>
+              </Container>
+      </BrowserRouter>
   );
 }
 
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import apiClient from './axiosConfig';
 
 function Home() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const response = await apiClient.post('/books/state/', {
+                    state: 'Florida',
+                });
+                console.log('Response:', response.data);
+                setBooks(response.data);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
+
+        fetchBooks();
+    }, []);
+
     return (
-        <label>Home Page</label>
-    )
+        <div>
+            <h1>Banned Books in Florida</h1>
+            <ul>
+                {books.length > 0 ? (
+                    books.map((book, index) => (
+                        <li key={index}>
+                            <pre>{JSON.stringify(book, null, 2)}</pre>
+                        </li>
+                    ))
+                ) : (
+                    <p>Couldn't get the books...</p>
+                )}
+            </ul>
+        </div>
+    );
 }
 
 export default Home;
