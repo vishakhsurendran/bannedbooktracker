ee47404d0d90a4eb9676115c63b8a2e40a218f32
hwait123 <hwait12@ufl.edu>
2025-04-20 13:33:36 -0400

    Added Reading List checkbox to Book Details.

--- front-end/src/pages/BookDetails.js
+++ front-end/src/pages/BookDetails.js
@@ -4,61 +4,13 @@ import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import apiClient from "../axios/axiosConfig";
 import { auth } from "../firebase/firebase";
+import ReadingListCheckbox from "../components/ReadingListCheckbox";
 
 const BookDetails = () => {
     const location = useLocation();
     const { book } = location.state || {};
     const navigate = useNavigate();
-    const [checkbox, setCheckbox] = useState(false);
     const [error, setError] = useState('')
-
-    useEffect(() => {
-        const handleCheckboxInitial = () => {
-            const uid = auth.currentUser.uid;
-            const response = apiClient.post('/user/get_reading_list/', {
-                user_id: uid
-                }).then(response =>{
-                    if (JSON.stringify(response).includes("\"id\":" + book.book_id)) {
-                    setCheckbox(true);
-                    console.log("Success!");
-                }
-                }).catch(err =>{
-                    console.log(err);
-                })
-        }; handleCheckboxInitial()
-    }, []);
-
-    const handleCheckboxChange = async () => {
-        if (!checkbox) {
-            try {
-                const token = await auth.currentUser.getIdToken();
-                const response = await apiClient.post('/user/add_reading_list/', {
-                    token: token,
-                    book_id: book.book_id,
-                    is_read: false
-                });
-            }
-            catch (err) {
-                setError('Error adding to reading list.');
-                console.log(error);
-            }
-        }
-        else if (checkbox) {
-            try {
-                const token = await auth.currentUser.getIdToken();
-                const response = await apiClient.post('/user/remove_reading_list/', {
-                    token: token,
-                    book_id: book.book_id,
-                });
-                console.log(response);
-            }
-            catch (err) {
-                setError('Error removing from reading list.');
-                console.log(error);
-            }
-        }
-        setCheckbox(!checkbox);
-    };
 
     if (!book) {
         return <p>No book data available.</p>
@@ -70,14 +22,7 @@ const BookDetails = () => {
             <p><strong>Type of Ban:</strong> {book.type_of_ban}</p>
             <p><strong>State:</strong> {book.state}</p>
             <p><strong>District:</strong> {book.district}</p>
-            <label>
-                <input
-                    type="checkbox"
-                    checked={checkbox}
-                    onChange={handleCheckboxChange}
-                />
-                {checkbox ? 'Added to Reading List' : 'Add to Reading List'}
-            </label>
+            <ReadingListCheckbox book={book}/>
             <button onClick={() => navigate('/search')}>Go Back to Search</button>
         </div>
     );
