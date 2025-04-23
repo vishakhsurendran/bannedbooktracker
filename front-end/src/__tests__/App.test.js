fcce20b5e026dff29b404cfd3f8dc50a974ced1e
Rebecca-L28 <lee.rebecca1@ufl.edu>
2025-04-23 19:45:07 -0400

    Search and Book Details tests

--- front-end/src/__tests__/App.test.js
+++ front-end/src/__tests__/App.test.js
@@ -1,10 +1,51 @@
 import { render, screen } from '@testing-library/react';
 import App from "../App"
 import {BrowserRouter} from "react-router";
+import BookDetails from '../pages/BookDetails'
+import {MemoryRouter} from 'react-router'
+import SearchPage from "../pages/SearchPage";
 
 //Test that the app is rendering correctly
 test('renders learn react link', () => {
   render(<BrowserRouter><App /></BrowserRouter>);
   const linkElement = screen.getByText(/HOME/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useLocation: () => ({
+    state: null
+  }),
+  useNavigate: () => jest.fn(),
+}));
+
+test('renders BookDetails component without book data', () => {
+  render(
+    <MemoryRouter>
+      <BookDetails />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText(/No book data available/i)).toBeInTheDocument();
+});
+
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useLocation: () => ({
+    search: '', // No query
+  }),
+}));
+
+test('renders SearchPage with no query', () => {
+  render(
+    <MemoryRouter>
+      <SearchPage />
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText(/Search Results for ""/i)).toBeInTheDocument();
+  expect(screen.getByText(/No results found./i)).toBeInTheDocument();
 });
