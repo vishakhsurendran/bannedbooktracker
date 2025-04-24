f288c748ffd6892916ea27c9b2deaa9229f1597f
Coleton O'Donnell <contact@coleton.io>
2025-04-23 21:09:33 -0400

    Revert "Frontend unit tests"

--- front-end/src/__tests__/Search.test.js
+++
@@ -1,9 +0,0 @@
-import apiClient from "../axios/axiosConfig";
-
-//Test that the API searches the database
-test('Search API works correctly', async () => {
-
-  const response = await apiClient.post('/books/title/', { title: "The Poet X" });
-  expect(response.data.length).toBeGreaterThan(0);
-
-});
